cb4ec9e9915966cf3ea3afd3d02401d9798984c3
Shubham <shubham.jawkar94@gmail.com>
2024-04-24 10:59:11 +0530

    first commit

---
+++ log-validation-utility/routes/validate/index.ts
@@ -0,0 +1,10 @@
+import express from 'express'
+import validateController from '../../controller/validate'
+
+const router = express.Router()
+
+router.post('/validate', validateController.validate)
+router.post('/validate/igm', validateController.validateIGM)
+router.post('/validate-schema', validateController.validateAction)
+
+export default router
